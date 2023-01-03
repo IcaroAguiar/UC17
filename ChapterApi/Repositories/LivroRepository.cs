6d09a4960aaad3ec7c76cbe3a7d42dee4e6d4564
IcaroAguiar <icaroaguiar14@gmail.com>
2023-01-03 11:25:27 -0300

    Primeiro commit - Encontro Remoto 1

---
+++ ChapterApi/Repositories/LivroRepository.cs
@@ -0,0 +1,22 @@
+﻿using ChapterApi.Contexts;
+using ChapterApi.Models;
+
+namespace ChapterApi.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly SqlContext _context;
+
+        public LivroRepository(SqlContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+
+        }
+    }
+
+}
