d138e63ed33cc8849ce7a332731be741b2a5d432
IcaroAguiar <icaroaguiar14@gmail.com>
2023-01-05 10:36:33 -0300

    Encontro remoto 2

--- ChapterApi/Controllers/LivroController.cs
+++ ChapterApi/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using ChapterApi.Repositories;
+﻿using ChapterApi.Models;
+using ChapterApi.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,7 +11,7 @@ namespace ChapterApi.Controllers
     public class LivroController : ControllerBase
     {
         private readonly LivroRepository _livroRepository;
-        public LivroController (LivroRepository livroRepository)
+        public LivroController(LivroRepository livroRepository)
         {
             _livroRepository = livroRepository;
         }
@@ -19,17 +20,94 @@ namespace ChapterApi.Controllers
 
         public IActionResult ler()
         {
-           
+
             try
             {
                 return Ok(_livroRepository.Listar());
             }
-            catch(Exception e)
+            catch (Exception e)
             {
-                throw new Exception( e.Message);
+                throw new Exception(e.Message);
             }
-            
+
 
         }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscaPorId(int id)
+        {
+            try
+            {
+
+                Livro livroBuscado = _livroRepository.BuscarPorId(id);
+
+                if (livroBuscado == null)
+                {
+                    return NotFound("Não encontrado");
+                }
+
+                return Ok(livroBuscado);
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro l)
+        {
+            try
+            {
+                _livroRepository.Cadastro(l);
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+
+
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return Ok("Livro Removido com sucesso");
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Aterar(int id, Livro l)
+        {
+            try
+            {
+                _livroRepository.Alterar(id, l);
+                return StatusCode(204);
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+
+
+
+        }
+
     }
 }
